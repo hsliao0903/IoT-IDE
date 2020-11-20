8408aeeec0116964c893e0c8d83b9e811b43ef10
hsliao0903 <alexliao800903@hotmail.com>
2020-11-20 16:30:58 +0800

    Finish the Listener Part

--- Program.cs
+++ Program.cs
@@ -3,45 +3,19 @@ using System.Text;
 using System.Threading;
 using System.Net;
 using System.Net.Sockets;
-
+using Newtonsoft.Json;
+using CNT5517_Project;
 
 namespace IoTIDE
 {
     class Program
     {
+        private SocketObj sockObj = new SocketObj();
+        private Thread tweetListenerThread;
+        private bool listenerAlive = false;
 
-        /* Try to receive Tweets from a multicast address */
-        static void ListenForTweets()
-        {
-            /* Multicast Address and Port which Atlas is using */
-            int multicastPort = 1235;
-            string multicastAddr = "232.1.1.1";
-
-            /* Create a unicast UDP socket (multicast uses UDP) */
-            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-
-            /* Create an endpoint to listen to, listen to a port on all interfaces */
-            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, multicastPort);
-            sock.Bind(ipep);
-
-            /* Add the socket to the multicast group */
-            IPAddress ip = IPAddress.Parse(multicastAddr);
-            sock.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(ip, IPAddress.Any));
-            Console.WriteLine("Start listening to Tweets at multicast address: " + multicastAddr + "/" + multicastPort);
-            while (true)
-            {
-                /* Deal with received Tweets */
-                byte[] buffer = new byte[1024];
-                if (sock.Receive(buffer) != 0)
-                {
-                    string str = Encoding.ASCII.GetString(buffer, 0, buffer.Length);
-                    Console.WriteLine(str.Trim());
-                    //TODO: Sotre the recevied Tweet in data structures
-                }
-            }
-        }
-
-        static void SendServiceCallTweets(string tweet, string ipAddr, int port)
+        
+        private void SendServiceCallTweets(string tweet, string ipAddr, int port)
         {
             Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ipAddr), port);
@@ -68,30 +42,125 @@ namespace IoTIDE
                     server.Close();
                     return;
                 }
+
             }
 
+        }
+
+
+
+        //private void connectBtn_Click(object sender, EventArgs e)
+        private void connectBtn_Click()
+        {
+            //connectBtn.Enabled = false;
+            //connectBtn.Text = "Start"
+            //serverSocket = new SocketObj(hostTextBox.Text, int.Parse(portTextBox.Text));
+            try
+            {
+                if (!listenerAlive)
+                {
+                    //connectBtn.Enabled = false;
+                    sockObj.DestroyListener(false);
+                    sockObj.SetListeningStatus(true);
+                    tweetListenerThread = new Thread(() => sockObj.ListenForTweets());
+                    tweetListenerThread.IsBackground = true;
+                    tweetListenerThread.Start();
+
+                    Console.WriteLine("Joining multicast address group 232.1.1.1/1235 ");
+                    /* pause for connection estalishing */
+                    Thread.Sleep(1000);
+
+
+
+                    Console.WriteLine("Successfully listening to multicast address group 232.1.1.1/1235");
+                    listenerAlive = true;
+                    //logTextBox.AppendText("Successfully connected to: " + hostTextBox.Text);
+                    //logTextBox.AppendText(Environment.NewLine);
+
+                    //logTextBox.AppendText("Establishing connection to multicast address 231.1.1.1/1235 ");
+                    //logTextBox.AppendText(Environment.NewLine);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Socket failed to join multicast address group 232.1.1.1/1235 ");
+                //logTextBox.AppendText("Connection failed to: " + hostTextBox.Text);
+                //logTextBox.AppendText(Environment.NewLine);
+                //connectBtn.Enabled = true;
+                //connectBtn.Focus();
+                return;
+            }
+
+
+
+
+        }
+
+        private void listeningBtn(bool listen_status)
+        {
+            if (!listen_status)
+            {
+                Console.WriteLine("Pause to receive Tweets");
+                sockObj.SetListeningStatus(false);
+            }
+            else
+            {
+                
+                Console.WriteLine("Resume to receive Tweets");
+                sockObj.SetListeningStatus(true);
+            }
+        }
+
+        private void destroyListener()
+        {
+            if (listenerAlive)
+            {
+                sockObj.DestroyListener(true);
+                listenerAlive = false;
+            }
         }
 
         static void Main(string[] args)
         {
             /* Listen to Tweets in the background as a thread */
-            Thread tweetListener = new Thread(new ThreadStart(ListenForTweets));
-            tweetListener.IsBackground = true;
-            tweetListener.Start();
+            //Thread tweetListener = new Thread(new ThreadStart(ListenForTweets));
+            //tweetListener.IsBackground = true;
+            //tweetListener.Start();
 
             string tweet = "{\"Tweet Type\":\"Service call\",\"Thing ID\":\"MyRPI_5341\"," +
                 "\"Space ID\":\"MySmartSpace\",\"Service Name\":\"RedLEDBlink\",\"Service Inputs\":\"(5)\"}";
-            string ipAddr = "192.168.3.55";
+            string ipAddr = "10.254.254.64";
             int port = 6668;
+
+            Program pp = new Program();
+            string inputStr;
             
             
+            while (true)
+            {
+                inputStr = Console.ReadLine();
+                if (inputStr == "connect")
+                    pp.connectBtn_Click();
+                else if (inputStr == "pause")
+                    pp.listeningBtn(false);
+                else if (inputStr == "resume")
+                    pp.listeningBtn(true);
+                else if (inputStr == "disconnect")
+                    pp.destroyListener();
+                else if (inputStr == "send")
+                    pp.SendServiceCallTweets(tweet, ipAddr, port);
+                else
+                    continue;
+
+            }
 
 
-            Console.WriteLine("Press <enter> to send Service Call Tweet");
-            Console.ReadLine();
-            SendServiceCallTweets(tweet, ipAddr, port);
-            Console.WriteLine("Press <enter> to quit");
-            Console.ReadLine();
+            //return;
+            //Console.WriteLine("Press <enter> to send Service Call Tweet");
+            //Console.ReadLine();
+            //SendServiceCallTweets(tweet, ipAddr, port);
+            //Console.WriteLine("Press <enter> to quit");
+            //Console.ReadLine();
         }
     }
 }
---
+++ SocketObj.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace CNT5517_Project
+{
+    class SocketObj
+    {
+        private int port;
+        private Socket socket;
+        private IPAddress Host;
+        private IPEndPoint ipe;
+        private bool connected = false;
+        private bool keepListening = false;
+        private bool destroyListenSocket = false;
+
+        public void ServerSocketObj(){}
+
+        /*
+        public void ServerSocketObj(string _host, int _port)
+        {
+            Host = IPAddress.Parse(_host);
+            port = _port;
+            ipe = new IPEndPoint(Host, port);
+            socket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        }
+        */
+        //public void ConnectSocket(){ socket.Connect(ipe);}
+
+        private void SetConnectionStatus() 
+        {
+            if(socket.Connected)
+            {
+                connected = true;
+            }
+        }
+
+       
+
+        private void CloseSocket()
+        {
+            try{socket.Shutdown(SocketShutdown.Both);}
+            finally{socket.Close();}
+        }
+
+        public bool GetConnectionStatus(){return connected;}
+
+        public void SetListeningStatus(bool status)
+        {
+            keepListening = status;
+        }
+
+        public void DestroyListener(bool status)
+        {
+            destroyListenSocket = status;
+        }
+
+        public void SendServiceCallTweets(string tweet, string ipAddr, int _port)
+        {
+            Host = IPAddress.Parse(ipAddr);
+            port = _port;
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ipe = new IPEndPoint(IPAddress.Parse(ipAddr), port);
+
+            try
+            {
+                socket.Connect(ipe);
+                SetConnectionStatus();
+                socket.Send(Encoding.ASCII.GetBytes(tweet));
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Unable to send Service Call Tweet to " + ipAddr);
+                return;
+            }
+            
+
+            byte[] response = new byte[1024];
+            if (socket.Receive(response) != 0)
+            {
+                string str = Encoding.ASCII.GetString(response, 0, response.Length);
+                Console.WriteLine("Response from " + ipAddr + ":\n" + str);
+                CloseSocket();
+                //socket.Close();
+            }
+
+
+        }
+
+
+        /* receive Tweets from a multicast address method */
+        public void ListenForTweets()
+        {
+            /* Multicast Address and Port which Atlas is using */
+            int multicastPort = 1235;
+            string multicastAddr = "232.1.1.1";
+
+            /* Create a unicast UDP socket (multicast uses UDP) */
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            /* Create an endpoint to listen to, listen to a port on all interfaces */
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, multicastPort);
+            sock.Bind(ipep);
+
+            /* Add the socket to the multicast group */
+            IPAddress ip = IPAddress.Parse(multicastAddr);
+            sock.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(ip, IPAddress.Any));
+
+            Console.WriteLine("Start listening to Tweets at multicast address: " + multicastAddr + "/" + multicastPort);
+            while (true)
+            {
+
+                if (destroyListenSocket)
+                {
+                    Console.WriteLine("Leave the multicast group1: " + multicastAddr + "/" + multicastPort);
+                    sock.Close();
+                    break;
+                }
+                /* Deal with received Tweets */
+
+                byte[] buffer = new byte[1024];
+                //if (sock.Receive(buffer) != 0)
+                if (sock.Available != 0)
+                {
+                    sock.Receive(buffer);
+                    string str = Encoding.ASCII.GetString(buffer, 0, buffer.Length);
+
+                    /* The socket is still there, but if user stop listening, we don't store the received Tweets */
+                    if (keepListening)
+                    {
+                        Console.WriteLine(str.Trim());
+                        //TODO: Sotre the recevied Tweet in data structures
+                    }
+
+                    if (destroyListenSocket)
+                    {
+                        Console.WriteLine("Leave the multicast group2: " + multicastAddr + "/" + multicastPort);
+                        sock.Close();
+                        break;
+                    }
+                }
+                
+
+            }
+        }
+
+
+    }
+}
